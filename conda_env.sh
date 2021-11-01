e1433b182dd8b438f67f91261067442e0971dab7
Peter Fogh <fogh@seges.dk>
2021-11-01 06:27:31 +0100

    Reran notebook

--- conda_env.sh
+++ conda_env.sh
@@ -1,2 +1,2 @@
 conda create -n dem_use_cases python=3.9
-conda install -n dem_use_cases ipykernel environs xarray rioxarray matplotlib hvplot geoviews
+conda install -n dem_use_cases ipykernel environs xarray rioxarray netcdf4 matplotlib hvplot geoviews
